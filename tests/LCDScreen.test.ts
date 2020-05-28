7996afd2a841b2b7184ed2c85c839a22c168d049
mm-devpro <mickael@mm-devpro.com>
2020-05-28 13:52:33 +0200

    implemented line command with prompt module. Use node ./command-line/get-input-from-console.js to input a value

--- tests/LCDScreen.test.ts
+++ tests/LCDScreen.test.ts
@@ -5,8 +5,10 @@ const screenTest = new LCDScreen();
 const getRandomInteger = (min, max) => {
     return Math.floor(Math.random()*(max - min + 1) + min)
 }
-test('display a number into console', () => {
+
+
+test('the method getInputIntoArray brings an object', () => {
     const randomInt = getRandomInteger(0, 9999999999);
-    expect(typeof screenTest.getInputIntoArray(randomInt)).toBe('array');
+    expect(typeof screenTest.getInputIntoArray(randomInt)).toBe('object');
 
 })
